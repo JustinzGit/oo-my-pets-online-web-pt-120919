92c07bf76d79884566fafd023e9b2b26a6aab3a8
Justin Branch <justinms@outlook.com>
2020-01-20 17:41:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -53,8 +53,8 @@ class Owner
   end 
   
   def sell_pets
-    cats.map!{|cat| cat.mood = "nervous" && cat.owner = nil}
-    dogs.map!{|dog| dog.mood = "nervous" && dog.owner = nil}
+    cats.map!{|cat| cat.mood = "nervous", cat.owner = nil}
+    dogs.map!{|dog| dog.mood = "nervous", dog.owner = nil}
   end 
 
     
