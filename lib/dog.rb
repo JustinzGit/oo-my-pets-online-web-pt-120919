8fc0c3c7360cf3f5f8b2894a75253bd39ac89810
Justin Branch <justinms@outlook.com>
2020-01-20 17:17:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,12 @@ class Dog
   attr_reader :name 
   attr_accessor :owner, :mood
   
+  @@all = []
+  
   def initialize(name, owner)
     @name = name 
     @owner = owner
     @mood = "nervous"
+    @@all << self
   end 
 end
