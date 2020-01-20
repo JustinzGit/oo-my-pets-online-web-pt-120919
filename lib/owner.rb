398d81d0f7a30718f8ecfdb7ef7a8ce88ba65c06
Justin Branch <justinms@outlook.com>
2020-01-20 17:11:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,15 @@
 class Owner
   attr_reader :name, :species
   
+  @@all = []
+  
   def initialize(name)
-    @name = name 
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
   end 
   
   def species(species="human")
